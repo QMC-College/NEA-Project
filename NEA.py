ca9fdbfcfc79b4a9668d10f7a2fee1666887180b
jeromeupload <jerome.shah@qmc.ac.uk>
2025-05-23 12:18:20 +0100

    Updated code

--- NEA.py
+++ NEA.py
@@ -1,4 +1,6 @@
 # This is my NEA Project
 print("This is my NEA project")
 name = input("Please enter your name: ")
-print(f"{name} is a cool name.")
+print(f"{name} is an awful name.")
+
+print("Ok, let's continue...")
